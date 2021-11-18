0de110e407f0b418c6d4b4dd04c30cfb225b2220
Wilson Gouanet <wilstito@gmail.com>
2021-11-18 07:16:41 -0500

    remove al compiled js files

--- components/timestamp/timestamp.router.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express_1 = require("express");
-var timestamp_controller_1 = require("./timestamp.controller");
-var TimestampRouter = (0, express_1.Router)();
-TimestampRouter.get('/', timestamp_controller_1["default"].getNow);
-TimestampRouter.get('/:date', timestamp_controller_1["default"].getFromParam);
-exports["default"] = TimestampRouter;
